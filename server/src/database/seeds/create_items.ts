eb4473894dbccaba59c8598ad9ea85ccc14d21cc
Kawan Jose da silva <kawan@MacBook-Pro-de-Kawan.local>
2020-06-10 16:27:43 -0300

    App

---
+++ server/src/database/seeds/create_items.ts
@@ -0,0 +1,12 @@
+import Knex from 'knex';
+
+export async function seed(knex: Knex) {
+    await knex('items').insert([
+        { title: 'Lâmpadas', image: 'lampadas.svg' },
+        { title: 'Pilhas e Baterias', image: 'baterias.svg' },
+        { title: 'Papéis e Palalão', image: 'papeis-papelao.svg' },
+        { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
+        { title: 'Resíduos Organicos', image: 'organicos.svg' },
+        { title: 'Óleo de cozinha', image: 'oleo.svg' },
+    ]);
+}
